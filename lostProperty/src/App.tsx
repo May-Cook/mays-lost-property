f85ade4cadcb50179c7a9352b806f929e701d928
may-cook <maycook.email@gmail.com>
2025-06-09 20:02:20 +0100

    Fix dropshadow

--- lostProperty/src/App.tsx
+++ lostProperty/src/App.tsx
@@ -5,7 +5,7 @@ function App() {
     <div className="min-h-screen bg-gradient-to-b from-pink-300 to-rose-400">
       <div className="h-20"></div>
       
-      <div className="bg-purpink-m border-purpink-d flex max-w-xl border-solid border-6 rounded-xl shadow-lg dark:shadow-pink-950/40 px-3 py-4 mx-auto my-5">
+      <div className="bg-purpink-m border-purpink-d flex max-w-xl border-solid border-6 rounded-xl shadow-lg shadow-pink-950/40 px-3 py-4 mx-auto my-5">
         <p className="text-stone-950 text-2xl text-center">
           <div className="font-semibold text-3xl">Hi, I'm May. </div>
           <div>
@@ -24,7 +24,7 @@ function App() {
         </p>
       </div>
       
-      <div className="bg-purpink-m border-purpink-d max-w-md border-solid border-6 shadow-lg dark:shadow-pink-950/40 rounded-xl px-3 py-4 mx-auto">
+      <div className="bg-purpink-m border-purpink-d max-w-md border-solid border-6 shadow-lg shadow-pink-950/40 rounded-xl px-3 py-4 mx-auto">
         <p className="text-stone-950 text-lg p-1 font-semibold">
           I don't want to give out too much of my info as I am worried I'm going to be contacted by spam/scammers, but here are some of my socials:
         </p>
@@ -49,7 +49,7 @@ function App() {
       </div>
 
       <div className="max-w-3xs py-5 mx-auto">
-        <img className="shadow-lg dark:shadow-pink-950/50 rounded-4xl" src={"src/assets/qrIcon.png"} alt="QR code sticker" />
+        <img className="shadow-lg shadow-pink-950/50 rounded-4xl" src={"src/assets/qrIcon.png"} alt="QR code sticker" />
       </div>
     </div>
   );
